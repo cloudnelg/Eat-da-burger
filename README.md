87777e9effbc90583a259171fbc26f504dc0938f
c1oud <cloudnelg@gmail.com>
2019-03-25 21:04:58 -0700

    Eat-da-burger

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Eat-da-burger
+# Eat Da Burger!
+Eat Burgers using 
+
+<ul>
+<li>Node.js</li>
+<li>Express</li>
+<li>MySQL</li>
+<li>Handlebars</li>
+</ul>
+
+
+
+## Usage
+
+Enter any burger name and add it to the menu / database. Your added burger will be listed as "available" on the left side of the screen. Eat burgers by clicking on it, moving it into the left column indicating the burger has been eaten.
+
+## Demo
+
+Demo this application [here]().
+
+
+
+
+
+
+
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+-- Insertion Data --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Classic Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Classic', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chili Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Burger', true);
+
